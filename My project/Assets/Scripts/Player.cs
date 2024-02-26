559f9d1af17bd21af9ad494f92c335e7edaddbd3
maddy415 <luis.maia@escolar.ifrn.edu.br>
2024-02-26 11:07:34 -0300

    Initial commit

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -7,6 +7,10 @@ public class NewBehaviourScript : MonoBehaviour
 
     public float Speed;
     public float JumpForce;
+
+    public bool isJumping;
+    public bool doubleJump;
+    
     private Rigidbody2D rig;
    
 
@@ -33,8 +37,32 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if(isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+        }
+    }
+        
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = false;
         }
     }
     
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = true;
+        }
+    }
 }
